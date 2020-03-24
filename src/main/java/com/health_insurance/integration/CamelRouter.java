ae9328b12fb185f9f5c49f8282853aec3f819723
Rafael T. C. Soares <rafaelcba@gmail.com>
2020-03-24 18:24:44 -0400

    first commit

---
+++ src/main/java/com/health_insurance/integration/CamelRouter.java
@@ -0,0 +1,134 @@
+package com.health_insurance.integration;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.health_insurance.phm_model.Response;
+import com.health_insurance.phm_model.Trigger;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Camel REST DSL route that implements 
+ * the integration between Kafka and Drools/jBPM.
+ * 
+ */
+@Component
+public class CamelRouter extends RouteBuilder {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CamelRouter.class);
+
+    @Value("${kie.decision.container.id}") 
+    String decisionContainerId;
+    @Value("${kie.decision.session.name}") 
+	String decisionSessionName;
+    @Value("${kie.process.container.id}") 
+    String processContainerId;
+    @Value("${kie.process.definition.id}") 
+	String processDefinitionId;
+    
+    @Value("${kafka.topic:test}") 
+	String kafkaTopic;
+    @Value("${kafka.host:localhost}") 
+	String kafkaHost;
+    @Value("${kafka.port:9092}") 
+    String kafkaPort;
+
+    @Value("${camel.seda.consumers}")
+    String sedaConsumers;
+    
+    private static final String KAFKA_SERIALIZER_CLASS_CONFIG = "org.apache.kafka.common.serialization.ByteArraySerializer";
+    private static final String KAFKA_DESERIALIZER_CLASS_CONFIG = "org.apache.kafka.common.serialization.ByteArrayDeserializer";
+    
+    @Override
+    public void configure() throws Exception {
+
+        // @formatter:off
+        restConfiguration()
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "Integration Service REST API")
+                .apiProperty("api.version", "1.0")
+                .apiProperty("cors", "true")
+                .apiProperty("base.path", "camel/")
+                .apiProperty("api.path", "/")
+                .apiProperty("host", "")
+                .apiContextRouteId("doc-api")
+            .component("servlet")
+            .bindingMode(RestBindingMode.json);
+        
+        rest("/trigger").description("Create a new Trigger and send it to Kafka Topic")
+            .consumes("application/json")
+            .produces("application/json")
+            .post().type(Trigger.class)
+                .route().routeId("trigger-api")
+                .to("direct:publishToKafka");
+
+    // Direct routes
+        from("direct:runKieCommand")
+            .routeId("kieServerClient")
+            .log("calling kie-server")
+            .to("bean:decisionServiceClient?method=listContainers")
+            .log("${body}");
+        
+        fromF("kafka:%s?brokers=%s:%s&valueDeserializer=%s", kafkaTopic, kafkaHost, kafkaPort, KAFKA_DESERIALIZER_CLASS_CONFIG)
+            .routeId("kafkaSubscriber")
+            .unmarshal().json(JsonLibrary.Jackson, Trigger.class)
+            .log("Message received from Kafka : ${body}")
+            .log("    on the topic ${headers[kafka.TOPIC]}")
+            .log("    on the partition ${headers[kafka.PARTITION]}")
+            .log("    with the offset ${headers[kafka.OFFSET]}")
+            .log("    with the key ${headers[kafka.KEY]}")  
+            .log("\n Call the decision server")
+            .to("seda:makeDecision");
+
+        fromF("seda:makeDecision?concurrentConsumers=%s", sedaConsumers)
+            .routeId("makeDecision")
+            .process(e -> {
+                LOG.debug("Decision request Body: " + e.getIn().getBody());
+                Trigger trigger = e.getIn().getBody(Trigger.class);
+
+                Map<String, Object> decisionFacts = new HashMap<>();
+                decisionFacts.put(Integer.toString(trigger.getTriggerId()), trigger);
+                e.getIn().setBody(decisionFacts);
+            }) // call decision service
+            .toF("bean:decisionServiceClient?method=executeCommands(%s, %s, ${body})", decisionContainerId, decisionSessionName)
+            .log("Decision Results: [ ${body} ]")
+            .to("seda:startProcess");
+
+        from("direct:publishToKafka")
+            .routeId("kafkaPublisher")
+            .marshal().json(JsonLibrary.Jackson, Trigger.class)
+            .log("publishing [ ${body} ] to kafka topic}")
+            .setHeader(KafkaConstants.KEY, constant("phm-trigger")) // Key of the message
+            .toF("kafka:%s?brokers=%s:%s&serializerClass=%s", kafkaTopic, kafkaHost, kafkaPort, KAFKA_SERIALIZER_CLASS_CONFIG);
+            
+        fromF("seda:startProcess?concurrentConsumers=%s", sedaConsumers)
+            .routeId("startProcess")
+            .process(e -> {
+                LOG.debug("Process request Body: " + e.getIn().getBody());
+
+                Map<String, Object> resultFactObjects = e.getIn().getBody(Map.class);
+                List<?> factsList = (List<?>)resultFactObjects.get("resultFactObjects");
+                List<?> responsesList = factsList.stream().filter(o -> o instanceof Response).collect(Collectors.toList());
+
+                Map<String, Object> processVariables = new HashMap<>();
+                processVariables.put("pDataList", responsesList);
+
+                e.getIn().setBody(processVariables);
+            }) // start a new Process instance
+            .toF("bean:processServiceClient?method=startProcess(%s, %s, ${body})", processContainerId, processDefinitionId)
+            .log("a process instance has been created with Id ${body}");
+        
+        // @formatter:on
+    }
+
+}
---
+++ src/main/java/com/health_insurance/kie/ProcessClient.java
@@ -0,0 +1,139 @@
+package com.health_insurance.kie;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import com.health_insurance.phm_model.Reminder;
+import com.health_insurance.phm_model.Response;
+import com.health_insurance.phm_model.Task;
+import com.health_insurance.phm_model.TaskActorAssignment;
+import com.health_insurance.phm_model.Trigger;
+
+import org.kie.server.api.marshalling.MarshallingFormat;
+import org.kie.server.api.model.KieContainerResource;
+import org.kie.server.api.model.KieContainerResourceList;
+import org.kie.server.api.model.KieServerInfo;
+import org.kie.server.api.model.definition.ProcessDefinition;
+import org.kie.server.client.KieServicesClient;
+import org.kie.server.client.KieServicesConfiguration;
+import org.kie.server.client.KieServicesFactory;
+import org.kie.server.client.ProcessServicesClient;
+import org.kie.server.client.QueryServicesClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+
+@Configuration
+@PropertySource("classpath:servers.properties")
+@Service("processServiceClient")
+public class ProcessClient {
+  private static final Logger LOG = LoggerFactory.getLogger(ProcessClient.class);
+
+  //TODO @Value doesn't read from the properties file
+
+  //@Value("${kie.processkieserver.url}")
+  //static String kieServerUrl;
+  //static String kieServerUrl = "http://localhost:8080/kie-server/services/rest/server";
+  static String kieServerUrl = "http://localhost:8091/rest/server";
+  //@Value("${kie.processkieserver.user}")
+  //static String kieServerUser;
+  static String kieServerUser = "kieserver";
+  //@Value("${kie.processkieserver.password}")
+  //static String kieServerPassword;
+  static String kieServerPassword = "kieserver1!";
+
+  private static final MarshallingFormat FORMAT = MarshallingFormat.JSON;
+  private static KieServicesClient kieServicesClient;
+
+  public ProcessClient() {
+    // Empty constructor.
+  }
+
+  @PostConstruct
+  public void initialize() {
+    final KieServicesConfiguration conf;
+    LOG.info("\n=== Initializing Kie Client ===\n");
+    LOG.info("\t connecting to {}", kieServerUrl);
+    conf = KieServicesFactory.newRestConfiguration(kieServerUrl, kieServerUser, kieServerPassword);
+
+    // If you use custom classes, such as Obj.class, add them to the configuration.
+    Set<Class<?>> extraClassList = new HashSet<Class<?>>();
+
+    //TODO: encapsulate this and expose to the callers
+    extraClassList.add(Task.class);
+    extraClassList.add(Reminder.class);
+    extraClassList.add(TaskActorAssignment.class);
+    extraClassList.add(Trigger.class);
+    extraClassList.add(Response.class);
+    conf.addExtraClasses(extraClassList);
+
+    conf.setMarshallingFormat(FORMAT);
+    kieServicesClient = KieServicesFactory.newKieServicesClient(conf);
+
+    listCapabilities();
+
+    LOG.info("=== Kie Client initialization done ===\n");
+  }
+
+  public List<String> listCapabilities() {
+    KieServerInfo serverInfo = kieServicesClient.getServerInfo().getResult();
+    LOG.info("Kie Server capabilities:");
+    serverInfo.getCapabilities().forEach(c -> LOG.info("\t" + c));
+    return serverInfo.getCapabilities();
+  }
+
+  public List<KieContainerResource> listContainers() {
+    KieContainerResourceList containersList = kieServicesClient.listContainers().getResult();
+    List<KieContainerResource> kieContainers = containersList.getContainers();
+    LOG.info("Available containers: ");
+    kieContainers.forEach(container ->
+      LOG.info("\t" + container.getContainerId() + " (" + container.getReleaseId() + ")")
+    );
+
+    return kieContainers;
+  }
+
+  public List<ProcessDefinition> listProcesses() {
+    LOG.info("== Listing Business Processes ==");
+    QueryServicesClient queryClient = kieServicesClient.getServicesClient(QueryServicesClient.class);
+    List<ProcessDefinition> processDefinitions = queryClient.findProcessesByContainerId("rewards", 0, 1000);
+    LOG.info("Available process: ");
+    processDefinitions.forEach(def -> 
+      LOG.info(def.getName() + " - " + def.getId() + " v" + def.getVersion())
+    );
+
+    return processDefinitions;
+  }  
+
+  /**
+   * Start a new process instance given a process definition and a map of process' variables
+   * 
+   * @param containerId kie container Id
+   * @param processDefinitionId process definition Id
+   * @param variables map of process input variables
+   * @return {@link Long} process instance id created
+   */
+  public Long startProcess(String containerId, String processDefinitionId, Map<String, Object> variables) {
+    LOG.info("== Sending commands to the kie-server [" + containerId + "] ==");
+    ProcessServicesClient processClient = kieServicesClient.getServicesClient(ProcessServicesClient.class);
+
+    LOG.info("\t Starting process [" + processDefinitionId + "] with the following input variables: ");
+    LOG.info("\t\t" + variables);
+    
+    return processClient.startProcess(containerId, processDefinitionId, variables);
+  }
+
+  @PreDestroy
+  public void closeResources(){
+    LOG.info("=== Kie Client finalization ===\n");
+    kieServicesClient.close();
+  }
+}
